e2b2bd7dfc3b65031d2b833cc0142745ce6200eb
Alexandre <alexandre_bernardi95@hotmail.com>
2024-02-02 14:43:07 -0300

    fix: adjust height button

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-const button = document.querySelector('.card-select-platform__title')
-const platforms = document.querySelector('.card-select-platform__buttons .card-select-platform__buttons__list')
+const button = document.querySelector(".card-select-platform__title");
+const platforms = document.querySelector(
+  ".card-select-platform__buttons .card-select-platform__buttons__list"
+);
 
+button.addEventListener("click", () => {
+  platforms.classList.toggle("active");
+});
 
-button.addEventListener( 'click', () => {
-  platforms.classList.toggle('active')
-})
-
-const selectPlatform = (e) =>{
-
-  const platform = e.target.textContent
-  button.textContent = platform
-  platforms.classList.remove('active')
-}
+const selectPlatform = (e) => {
+  const platform = e.target.textContent;
+  button.textContent = platform;
+  platforms.classList.remove("active");
+};
